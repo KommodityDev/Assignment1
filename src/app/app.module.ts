407813111abd9812b2c67b42fc5277a124bf53ca
KommodityDev <cacwilliams27@gmail.com>
2022-05-24 11:27:12 -0400

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, WarningAlertComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
